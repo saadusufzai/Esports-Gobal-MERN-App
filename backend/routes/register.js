6f4f6d171267209c09bfb00127a9173fa2bb3109
Saad Aslam <saadusufzai@gmail.com>
2020-10-16 05:13:32 +0500

    Backend

---
+++ backend/routes/register.js
@@ -0,0 +1,32 @@
+const router = require('express').Router()
+let Register = require('../models/register.model')
+
+router.route('/').post((req,res) => {
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
+    const email = req.body.email
+    const password = req.body.password
+    const country = req.body.country
+    const city = req.body.city
+    const pubgId = req.body.pubgId
+    const phone = req.body.phone
+
+    const newRegister = new Register({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        country: country,
+        city: city,
+        pubgId: pubgId,
+        phone: phone
+    })
+
+
+    newRegister
+    .save()
+    .then(()=> res.json('User Registered'))
+    .catch((err)=> res.status(400).json('Error: ' + err))
+})
+
+module.exports = router
