5f426c004ba07a2630fe3d4f9f6504e85ecacd25
Saad Aslam <saadusufzai@gmail.com>
2020-10-19 11:43:02 +0500

    rules and admin css updated

--- src/components/rules/rules.js
+++ src/components/rules/rules.js
@@ -8,7 +8,7 @@ export const rules = [
     "Any player who will be caught teaming up will be blacklisted from the Tournament.",
     "Only those players will be provided with the room ID and Password who are registered here.",
     "Unregistered Players will be Kicked out of the room before starting the match.",
-    "Emeulator Players are not allowed to Play. If any one will join using emeulator, will be kicked out.",
+    "Emulator Players are not allowed to Play. If any one will join using Emulator, will be kicked out.",
     "Winners will get their reward withn one hour after the match.",
     "Esports Global team will be spectating and recording the tournament all the time.",
     "Any player caught cheating or using any kind of cheats will be banned and kicked out immidiately without intimation. ",
