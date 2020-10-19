e61ddd15833641060d0174d315d2c38a819e2ef2
Saad Aslam <saadusufzai@gmail.com>
2020-10-19 18:43:06 +0500

    Styles Updated

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -6,6 +6,16 @@ import { useNavigate } from "react-router-dom";
 const Header = () => {
   const navigate = useNavigate();
   const [color, setColor] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+ 
+
+  const handelMenuOpen = () => {
+    setMenuOpen(true);
+  };
+  const handelMenuClose = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <div className="header">
       <div className="logo">
@@ -15,9 +25,25 @@ const Header = () => {
           src={logo}
           alt="logo-esports-global"
         />
+        <i
+          onClick={() => handelMenuOpen()}
+          class="menu-btn fa fa-bars"
+          aria-hidden="true"
+        ></i>
       </div>
-      <div className="menu">
+      <div
+        style={{
+          transform: `${ menuOpen ? "translateX(0)" : "translateX(-110)"}`,
+        }}
+        className="menu"
+      >
         <ul>
+          <i
+            onClick={() => handelMenuClose()}
+            class="close-btn fa fa-times"
+            aria-hidden="true"
+          ></i>
+
           <li>
             <a href="/">HOME</a>
           </li>
@@ -27,9 +53,8 @@ const Header = () => {
           <li>
             <a href="/tournaments">TOURNAMENTS</a>
           </li>
-          <li style={{background:"red",}} className="btn">
+          <li style={{ background: "red" }} className="btn">
             <a
-              
               onMouseEnter={() => setColor(true)}
               onMouseLeave={() => setColor(false)}
               style={{ color: `${color ? "#000" : "#fff"}` }}
