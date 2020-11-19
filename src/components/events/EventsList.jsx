a592e96468d5497fc1a0b0d66de6de6a4f16d04a
Saad Aslam <saadusufzai@gmail.com>
2020-11-19 19:37:53 +0500

    Update EventsList.jsx

--- src/components/events/EventsList.jsx
+++ src/components/events/EventsList.jsx
@@ -20,8 +20,8 @@ const EventsList = () => {
         <div className="summary">
           <span>
             <i className="fa fa-calendar-check-o" aria-hidden="true"></i>
-            November 15, 2020 :
-            <i className="fa fa-clock-o" aria-hidden="true"></i> 08:30 PM
+            November 22, 2020 :
+            <i className="fa fa-clock-o" aria-hidden="true"></i> 09:15 PM
           </span>
           <span>
             <i className="fa fa-users" aria-hidden="true"></i>VS
@@ -46,26 +46,26 @@ const EventsList = () => {
       <br />
       <br />
       <div className="inner-events">
-        <h2>Solo Vs Solo PUBG MOBILE FREE Tournament </h2>
+        <h2>Squad Vs Squad PUBG MOBILE Tournament </h2>
         <div className="summary">
           <span>
-          <i className="fa fa-calendar-check-o" aria-hidden="true"></i>
-            November 8, 2020 :
-            <i className="fa fa-clock-o" aria-hidden="true"></i> 08:00 PM
+            <i className="fa fa-calendar-check-o" aria-hidden="true"></i>
+            November 15, 2020 :
+            <i className="fa fa-clock-o" aria-hidden="true"></i> 08:30 PM
           </span>
           <span>
-            <i className="fa fa-male" aria-hidden="true"></i>VS
-            <i className="fa fa-male" aria-hidden="true"></i>
+            <i className="fa fa-users" aria-hidden="true"></i>VS
+            <i className="fa fa-users" aria-hidden="true"></i>
           </span>
         </div>
         <div className="text">
           <img src={img} alt="pubg" />
           <p>
-            Esports Global is organizing a Solo vs Solo 1 Match PUBG Mobile
-            Tournament. Any one can participate and get a chance to win 660UC or
-            1500 PKR. Top player will win a 660UC or 1500 PKR. The
+            Esports Global is organizing a Squad vs Squad 1 Match PUBG Mobile
+            Tournament.Only Mobile players can participate can participate and get a chance to win 6000 PKR. The
             registration process is really simple. just Click the register
-            button and thats it.This tournament is FREE, No Registration charges.
+            button and thats it. Registration charges are only 800Rs (200 per/member) for this
+            Tournament.
           </p>
         </div>
         <div onClick={() => handelOnRegister()} className="btn">
